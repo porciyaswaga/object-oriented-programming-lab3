b522a9407b8f6609efb03111d3d2a6f0cf359699
Арсений <aareh106@gmail.com>
2025-12-09 15:46:48 +0300

    Create Square.h

---
+++ include/Square.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Figure.h"
+#include <array>
+
+class Square : public Figure {
+private:
+    std::array<std::pair<double, double>, 4> vertices;
+
+public:
+    Square() = default;
+    Square(const std::array<std::pair<double, double>, 4>& verts);
+
+    std::pair<double, double> geometricCenter() const override;
+    double area() const override;
+    void printVertices(std::ostream& os) const override;
+    void readVertices(std::istream& is) override;
+
+    Figure& operator=(const Figure& other) override;
+    bool operator==(const Figure& other) const override;
+    Square& operator=(const Square& other);
+};
